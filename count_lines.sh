3c1108c80f8272fb65fb4c0829abf200d3971272
Michael Kubina <michael.kubina@sub.uni-hamburg.de>
2021-11-30 10:00:59 +0100

    add line counter script

---
+++ count_lines.sh
@@ -0,0 +1,7 @@
+# Ein kleins Script zum Zählen von Zeilen in TSV-Dateien
+# 2021-11-29 - Michael Kubina <michael.kubina@sub.uni-hamburg.de>
+
+echo "Count lines in TSV files"
+wc -l *.tsv
+echo "Done! Have a nice day!"
+
